7e45cb63aff7566b5b7beffb68e41aeecdd7362d
Ignas-Cij <75612565+Ignas-Cij@users.noreply.github.com>
2021-11-21 20:37:57 +0200

    not done

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,9 +9,9 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
 
   getWeather(location: string){
-      const path = `http://api.weatherstack.com/current
-      ? access_key = fa2bb5cac9a4eff4e13ee93bc80a6cd2
-      & query = ${location}`;
+      const path = `http://api.weatherstack.com/current?
+      access_key=23f8c7688b8b65c86bf439e521169c24
+      &query=New%20York`;
       return this.http.get(path);
   } 
 }
